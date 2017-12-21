b4101c1321410e447db4b445f757f380f10f2f78
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2017-12-21 13:02:03 -0500

    initial javascript ticketmaster api

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 
-	var url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=RElm0QfyEntLwlAvZwiZBD5GExqBRGIO&postalCode=10002";
 
-		var url1 = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=RElm0QfyEntLwlAvZwiZBD5GExqBRGIO";
+var apiKey = "RElm0QfyEntLwlAvZwiZBD5GExqBRGIO"
 
-		var apikey  = "RElm0QfyEntLwlAvZwiZBD5GExqBRGIO";
+// zip code for NY, needs to be changed to the user input
+var zipCode = "&postalCode=" + "10002"
+
+var keyword = "&keyword=" + "" //user keyword from dropdown
+
+var radius = "&radius=" + "15" //user radius input
+
+var classificationName = "&classificationName=music"
+
+var queryUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apiKey + zipCode + keyword + classificationName;
+
+
+
+var apikey  = "RElm0QfyEntLwlAvZwiZBD5GExqBRGIO";
+
+
 
 		$.ajax({
 			type:"GET",
-			url:url,
+			url:queryUrl,
 			async:true,
 			dataType: "json",
 			success: function(json) {
