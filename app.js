10e5bc5f902da05f21498d38937b4cc5f567261d
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2017-12-21 14:07:03 -0500

    queryUrl based on user input

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
+$(document).ready(function(){
+
+ $("#submit").on("click", function(event) {
+
+ 	event.preventDefault();
+
+	var searchInput = $("#search-input").val();
+
+	// value from dropdown
+
+	var eventInput = $('#eventType-input').find(":selected").text();
+	console.log("eventInput" + eventInput)
+	
+	var cityInput = $("#city-input").val();
+	var stateInput = $("#state-input").val();
+	var zipInput = $("#zip-input").val();
+	var radiusInput = $("#radius-input").val();
 
 
-var apiKey = "RElm0QfyEntLwlAvZwiZBD5GExqBRGIO"
+	var apiKey = "RElm0QfyEntLwlAvZwiZBD5GExqBRGIO"
 
-// zip code for NY, needs to be changed to the user input
-var zipCode = "&postalCode=" + "10002"
+	var zipCode = "&postalCode=" + zipInput;
 
-var keyword = "&keyword=" + "" //user keyword from dropdown
+	var keyword = "&keyword=" + searchInput; 
 
-var radius = "&radius=" + "15" //user radius input
+	var radius = "&radius=" + radiusInput; 
 
-var classificationName = "&classificationName=music"
+	var classificationName = "&classificationName=" + eventInput;
 
-var queryUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apiKey + zipCode + keyword + classificationName;
-
+	var queryUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apiKey + zipCode + keyword + classificationName;
 
 
-var apikey  = "RElm0QfyEntLwlAvZwiZBD5GExqBRGIO";
+
+	var apikey  = "RElm0QfyEntLwlAvZwiZBD5GExqBRGIO";
 
 
 
@@ -33,4 +49,8 @@ var apikey  = "RElm0QfyEntLwlAvZwiZBD5GExqBRGIO";
               // This time, we do not end up here!
           }
       });
+
+	});
+
+});
 
