1cc3961af45c8feccded5b9aa2811786cdd8c8cd
Lisa Zapson <lisazapson@ymail.com>
2017-12-28 11:18:07 -0500

    html jss changes

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
+var locations =[];
 
-	var locations =[];
+var marker;
 
-	var marker;
+function initGoogleMap() {
+	console.log(locations);
+	var map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 15,
+		center: {lat: locations[0].lat, lng: locations[0].lng}
+	})
 
- 	function initGoogleMap() {
- 		console.log(locations);
-		var map = new google.maps.Map(document.getElementById("map"), {
-			zoom: 15,
-			center: locations[0]
-		})
+	for (var k = 0; k < locations.length; k++) {
+		marker = new google.maps.Marker({
+			position: {lat: locations[k].lat, lng: locations[k].lng},
+			map: map,
+			url: locations[k].url
+		});
+		marker.addListener('click', function() {
+			window.open(this.url, "_blank");
+		});                        
+	}    
+}; // initGoogleMap
 
-		for (var k = 0; k < locations.length; k++) {
-				marker = new google.maps.Marker({
-				position: locations[k],
-				map: map
-			});						
-		}			
-	};
 
 $(document).ready(function(){
 
@@ -28,6 +32,89 @@ $(document).ready(function(){
 
 	var events;
 
+	var apiKey;
+	var city
+	var state;
+	var zipCode;
+	var keyword; 
+	var radius; 
+	var classificationName;
+	var size;
+	var page;
+
+	var queryUrl;
+
+	var numPerPage = 10;
+	var totalEvents;
+	var currentPage = 0;
+
+	var searchObj = {
+			keyword : "",
+			category : "",
+			city : "",
+			state : "",
+			zip : "",
+			radius : 5,
+			fromDate : moment(),
+			toDate : moment()
+		}
+
+
+	console.log("numperpage" + numPerPage)
+
+
+	function setSearchParameters() {
+		// keyword
+		searchObj.keyword = $("#keyword-input").val();
+
+		// Category
+		searchObj.category = "";
+		if ($("#categories-selected")) {
+			searchObj.category  = $("#categories-selected").val();
+			searchObj.category = getCategory(searchObj.category);
+		} 
+
+		// city
+		searchObj.city = $("#city-input").val();
+
+		// state
+		searchObj.state = $("#state-input option:selected").val();
+
+		// zip
+		searchObj.zip = $("#zip-input").val();
+
+		// radius
+		searchObj.radius = $("#radius-input").val();
+
+		// from date
+		searchObj.fromDate = moment();
+
+		// to date
+		searchObj.toDate = moment().add(7, 'days');
+
+		console.log(searchObj);
+
+	} // setSearchParameters
+
+	function getCategory(categoryValue) {
+		switch(categoryValue) {
+			case 100:
+			return "music";
+			break;
+			case 200:
+			return "sports"
+			break;
+			case 300:
+			return "family";
+			break;
+			case 400:
+			return "arts-theater"
+			break;
+			default:
+			return "";
+		}
+	} // getCategory
+
 	function clearLocations() {
 		locations = [];
 	}
@@ -36,32 +123,10 @@ $(document).ready(function(){
 		$("#event-list").empty();
 	}
 
- $("#submit").on("click", function(event) {
- 	clearLocations();
+	function ajaxCall(){
 
- 	clearEvents();
-
- 	event.preventDefault();
-    var searchInput = $("#search-input").val();
-    // value from dropdown
-    var eventInput = $('#eventType-input').find(":selected").attr("value");
-    console.log("eventInput" + eventInput)
-    
-    var cityInput = $("#city-input").val();
-    //&stateCode=il //&city=chicago
-    var stateInput = $('#state-input').find(":selected").attr("value");
-    var zipInput = $("#zip-input").val();
-    var radiusInput = $("#radius-input").val();
-    var apiKey = "RElm0QfyEntLwlAvZwiZBD5GExqBRGIO"
-    var city = "&city=" + cityInput;
-    var state = "&stateCode=" + stateInput;
-    var zipCode = "&postalCode=" + zipInput;
-    var keyword = "&keyword=" + searchInput; 
-    var radius = "&radius=" + radiusInput; 
-    var classificationName = "&classificationName=" + eventInput;
-   
-    var queryUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apiKey + zipCode + city + state + radius+  keyword + classificationName;
-
+		clearEvents();
+		clearLocations();
 
 		$.ajax({
 			type:"GET",
@@ -70,7 +135,6 @@ $(document).ready(function(){
 			dataType: "json",
 			success: function(json) {
 				console.log(json);
-
 
 				if(json.page.totalElements === 0) {
 					alert("Sorry! There are no events in your area!");
@@ -88,100 +152,233 @@ $(document).ready(function(){
 							}
 							else {
 
-								var latLong = {lat: parseFloat(venues[j].location.latitude), lng: parseFloat(venues[j].location.longitude)};
+								var latLong = {lat: parseFloat(venues[j].location.latitude), lng: parseFloat(venues[j].location.longitude), url: "https://www.google.com/maps/dir/?api=1&map_action=map&destination=" + venues[j].location.latitude + "%2C+" + venues[j].location.longitude};
 								console.log(latLong);
 								locations.push(latLong);
 							}
 						}
 					}
-				}
+
+
+					//display the information
+
+					for (var m = 0; m < events.length; m++) {
+
+						console.log("event name:" + events[m].name);
+						console.log("date and time: " + events[m].dates.start.localDate + " " + events[m].dates.start.localTime)
+						console.log("venue: " + events[m]._embedded.venues[0].name)
+						console.log("Address: " + events[m]._embedded.venues[0].address.line1)
+						console.log(events[m]._embedded.venues[0].city.name + ", " + 
+							events[m]._embedded.venues[0].state.stateCode + " " +
+							events[m]._embedded.venues[0].postalCode )
+						console.log("url: " + events[m].url )
+
+						var eventDisplay = $("<div>").addClass("event");
+
+						var eventUrl = $("<a>").addClass("event-link");
+						eventUrl.attr("href", events[m].url)
+						eventUrl.attr("target", "_blank")
+						
+						var eventName = $("<span>").addClass("name-display");
+						eventName.append(events[m].name);
+						eventUrl.append(eventName);
+
+						var par = $("<p>");
+
+						var eventDate = $("<span>").addClass("date-display");
+						var formattedDate = (moment(events[m].dates.start.localDate).format("ddd MM/DD/YY"));
+						var time = events[m].dates.start.localTime;
+						var convertedTime = moment(time, "HH:mm:ss")
+						eventDate.append(formattedDate + " " + moment(convertedTime).format("h:mm A"));
+
+						var eventVenue = $("<span>").addClass("venue-display");
+						eventVenue.append("<br>"+ events[m]._embedded.venues[0].name);
+
+						var eventAddress = $("<span>").addClass("venu-address1-display");
+						eventAddress.append("<br>" + events[m]._embedded.venues[0].address.line1);
+						
+						var eventCity = $("<span>").addClass("venue-city-display");
+						eventCity.append("<br>" + events[m]._embedded.venues[0].city.name + ", " + 
+							events[m]._embedded.venues[0].state.stateCode + " " +
+							events[m]._embedded.venues[0].postalCode);
+
+						par.append(eventDate, eventVenue, eventAddress, eventCity);
+						par.addClass("par-display");
+						var eventPic = $("<img src=" + events[m].images[2].url + " alt=" + events[m].name + ">");
+						eventPic.addClass("image-display");
+						eventDisplay.append(eventUrl, par, eventPic);
+
+						$("#event-list").append(eventDisplay);
+
+
+						totalEvents = json.page.totalElements;
+
+
+						// console.log(moment(events[m].dates.start.localDate).format("dddd"));
+						console.log(moment(events[m].dates.start.localDate).format("ddd MM/DD/YY"));
+						console.log(moment(time, "HH:mm:ss"))
+						console.log(moment(convertedTime).format("h:mm A"))
+
+
+
+						
+
+					}
+
+					// to prevent additional pagination each time
+					$(".pagination").empty();
+
+					console.log(totalEvents + " Total Events")
+					if (totalEvents > numPerPage){
+						var numPages;
+						if (totalEvents < numPerPage*10){
+							numPages = Math.ceil(totalEvents/numPerPage)
+							console.log(numPages + " numPages");
+						}
+
+						else{
+							numPages = 10;
+							console.log(numPages + " numPages");
+						}
+
+
+						for (var q = 0; q < numPages; q++) {
+							var pageList = $("<li class='page-item'><a class='page-link'>" + parseInt(q+1) + "</a></li>");
+							pageList.attr("data-number", q);
+							if (q==currentPage){
+								pageList.addClass("active")
+							}
+
+							pageList.attr("id", "page-" + parseInt(q+1))
+							$(".pagination").append(pageList)
+
+						}
+
+    			} // close if totalEvents > numPerPage (pagination)
+
+
+				} // close the else --> when there are events in the response
+
 				initGoogleMap();
 
-              // Parse the response.
-              // Do other things.
+
 
 				console.log("queryUrl" + queryUrl);
-              // Parse the response.
-              // Do other things.
-          
-        //display the information
-				events = json._embedded.events;
-
-				for (var m = 0; m < events.length; m++) {
-
-					console.log("event name:" + events[m].name);
-					console.log("date and time: " + events[m].dates.start.localDate + " " + events[m].dates.start.localTime)
-					console.log("venue: " + events[m]._embedded.venues[0].name)
-					console.log("Address: " + events[m]._embedded.venues[0].address.line1)
-					console.log(events[m]._embedded.venues[0].city.name + ", " + 
-					events[m]._embedded.venues[0].state.stateCode + " " +
-					events[m]._embedded.venues[0].postalCode )
-					console.log("url: " + events[m].url )
-
-					var eventDisplay = $("<div>").addClass("event");
-
-					var eventUrl = $("<a>").addClass("event-link");
-					eventUrl.attr("href", events[m].url)
-					eventUrl.attr("target", "_blank")
-					
-					var eventName = $("<span>").addClass("name-display");
-					eventName.append(events[m].name);
-					eventUrl.append(eventName);
-
-					var par = $("<p>");
-
-					var eventDate = $("<span>").addClass("date-display");
-					var formattedDate = (moment(events[m].dates.start.localDate).format("ddd MM/DD/YY"));
-					eventDate.append(formattedDate + " " + events[m].dates.start.localTime);
-
-					var eventVenue = $("<span>").addClass("venue-display");
-					eventVenue.append("<br>"+ events[m]._embedded.venues[0].name);
-
-					var eventAddress = $("<span>").addClass("venu-address1-display");
-					eventAddress.append("<br>" + events[m]._embedded.venues[0].address.line1);
-					
-					var eventCity = $("<span>").addClass("venue-city-display");
-					eventCity.append("<br>" + events[m]._embedded.venues[0].city.name + ", " + 
-					events[m]._embedded.venues[0].state.stateCode + " " +
-					events[m]._embedded.venues[0].postalCode);
-
-					par.append(eventDate, eventVenue, eventAddress, eventCity);
-					par.addClass("par-display");
-					var eventPic = $("<img src=" + events[m].images[2].url + " alt=" + events[m].name + ">");
-					eventPic.addClass("image-display");
-					eventDisplay.append(eventUrl, par, eventPic);
-
-					$("#event-list").append(eventDisplay);
-
-
-				// console.log(moment(events[m].dates.start.localDate).format("dddd"));
-				console.log(moment(events[m].dates.start.localDate).format("ddd MM/DD/YY"));
-				var time = events[m].dates.start.localTime;
-				console.log(moment.parseZone(time)._i);
-				console.log("time: " + time)
-				console.log(moment(time, "HH:mm a"));
 
 
 
-					// eventDisplay.append(events[m].dates.start.localDate + " " + events[m].dates.start.localTime);
-					// eventDisplay.append("venue: "  +events[m]._embedded.venues[0].name);
-					// eventDisplay.append("Address: " + events[m]._embedded.venues[0].address.line1);
-					// eventDisplay.append(events[m]._embedded.venues[0].city.name + ", " + 
-					// events[m]._embedded.venues[0].state.stateCode + " " +
-					// events[m]._embedded.venues[0].postalCode)
-
-
-
-					// $("#event-list").append(eventDisplay);
-				}
-
-          },
+          }, //close success
           error: function(xhr, status, err) {
               // This time, we do not end up here!
           }
-      });
-		 console.log("locations: " + [locations])
-	});
-});
+      }); // close ajax
+
+	} //close function definition
 
 
+
+	$("#submit").on("click", function(event) {
+		event.preventDefault();
+
+		setSearchParameters();
+
+		clearLocations();
+
+		//every time there is a new search, go to first page of results
+		currentPage = 0;
+
+		// clearEvents();
+
+		apiKey = "RElm0QfyEntLwlAvZwiZBD5GExqBRGIO"
+		city = "&city=" + searchObj.city;
+		state = "&stateCode=" + searchObj.state;
+		zipCode = "&postalCode=" + searchObj.zip;
+		keyword = "&keyword=" + searchObj.keyword; 
+		radius = "&radius=" + searchObj.radius; 
+		classificationName = "&classificationName=" + searchObj.category;
+
+
+		size = "&size=" + numPerPage;
+		page = "&page=" + currentPage;
+
+		queryUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apiKey + zipCode + city + state + radius+  keyword + classificationName + size + page;
+
+		ajaxCall();
+
+
+		console.log("locations: " + [locations])
+
+	}); //submit click
+
+
+	$("#page-buttons").on("click", "li", function() {
+		currentPage = $(this).attr("data-number");
+
+		console.log("currentPage " + currentPage)
+
+
+
+		// // console.log(($(this).val) + "this.val()")
+		page = "&page=" + currentPage
+
+		// remove active class from all page numbers 
+		//(this way we don't have to determine which page number was the last one clicked)
+		$(".page-item").removeClass("active");
+		//add class "active" to the page numebr clicked
+		$(this).addClass("active");
+		queryUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apiKey + zipCode + city + state + radius+  keyword + classificationName + size + page;
+
+		//ajaxCall with the new page number in queryUrl
+		ajaxCall();
+
+	}); //page-button click
+
+
+	// Search  - modal popups
+	$('.modal').modal({
+	      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+	      opacity: 0, // Opacity of modal background
+	      inDuration: 300, // Transition in duration
+	      outDuration: 200, // Transition out duration
+	      startingTop: '4%', // Starting top style attribute
+	      endingTop: '15%', // Ending top style attribute
+	      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+	      	console.log(modal, trigger);
+	      },
+	      complete: function() { 
+
+	      } // Callback for Modal close
+ 	 });
+
+
+	// Search - form select
+	$('select').material_select();
+
+
+   //  $('.datepicker').pickadate({
+	  //   selectMonths: true, // Creates a dropdown to control month
+	  //   selectYears: 15, // Creates a dropdown of 15 years to control year,
+	  //   today: 'Today',
+	  //   clear: 'Clear',
+	  //   close: 'Ok',
+	  //   closeOnSelect: false // Close upon selecting a date,
+  	// });
+
+  	// Search - categories
+  	$(document).on("click", "#categories", function() {
+  		var selected = $("#categories-selected");
+
+  		if (selected) {
+  			selected.attr("id", "categories");
+  		}
+  		$(this).attr("id", "categories-selected");
+  	});
+
+  	$(document).on("click", "#categories-selected", function() {
+  		$(this).attr("id", "categories");
+  	});
+
+
+
+
+}); //document.ready
