e722710e3c62a2a1ad0407bd5dd5b51e9be2830f
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2017-12-30 23:27:33 -0500

    fix

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var locations =[];
 
 var marker;
@@ -22,33 +22,7 @@ function initGoogleMap() {
 	}    
 }; // initGoogleMap
 
-=======
 
-	var locations =[];
-
-	var marker;
-
- 	function initGoogleMap() {
-         console.log(locations);
-        var map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 15,
-            center: {lat: locations[0].lat, lng: locations[0].lng}
-        })
-
-        for (var k = 0; k < locations.length; k++) {
-                marker = new google.maps.Marker({
-                position: {lat: locations[k].lat, lng: locations[k].lng},
-                map: map,
-                url: locations[k].url
-            });
-            marker.addListener('click', function() {
-                window.open(this.url, "_blank");
-               });                        
-        }    
-    };
-
-	
->>>>>>> 00f5d66c54959d56b77347548b1594829bb29e78
 $(document).ready(function(){
 
 	var string = "1.6525625";
@@ -58,8 +32,6 @@ $(document).ready(function(){
 	console.log(number);
 
 	var events;
-
-<<<<<<< HEAD
 	var apiKey;
 	var city
 	var state;
@@ -339,37 +311,6 @@ $(document).ready(function(){
 	}
 
 
-=======
-	var searchInput;
-  var eventInput;
-
-  
-  var cityInput;
-  
-  var stateInput;
-  var zipInput;
-  var radiusInput;
-  var apiKey;
-  var city
-  var state;
-  var zipCode;
-  var keyword; 
-  var radius; 
-  var classificationName;
-  var size;
-  var page;
- 
-  var queryUrl;
-
-  var numPerPage = 10;
-  var totalEvents;
-  var currentPage = 0;
-  
-
-  console.log("numperpage" + numPerPage)
-
- 
->>>>>>> 00f5d66c54959d56b77347548b1594829bb29e78
 	function clearLocations() {
 		locations = [];
 	}
@@ -424,13 +365,8 @@ $(document).ready(function(){
 						console.log("venue: " + events[m]._embedded.venues[0].name)
 						console.log("Address: " + events[m]._embedded.venues[0].address.line1)
 						console.log(events[m]._embedded.venues[0].city.name + ", " + 
-<<<<<<< HEAD
 							events[m]._embedded.venues[0].state.stateCode + " " +
 							events[m]._embedded.venues[0].postalCode )
-=======
-						events[m]._embedded.venues[0].state.stateCode + " " +
-						events[m]._embedded.venues[0].postalCode )
->>>>>>> 00f5d66c54959d56b77347548b1594829bb29e78
 						console.log("url: " + events[m].url )
 
 						var eventDisplay = $("<div>").addClass("event");
@@ -459,13 +395,8 @@ $(document).ready(function(){
 						
 						var eventCity = $("<span>").addClass("venue-city-display");
 						eventCity.append("<br>" + events[m]._embedded.venues[0].city.name + ", " + 
-<<<<<<< HEAD
 							events[m]._embedded.venues[0].state.stateCode + " " +
 							events[m]._embedded.venues[0].postalCode);
-=======
-						events[m]._embedded.venues[0].state.stateCode + " " +
-						events[m]._embedded.venues[0].postalCode);
->>>>>>> 00f5d66c54959d56b77347548b1594829bb29e78
 
 						par.append(eventDate, eventVenue, eventAddress, eventCity);
 						par.addClass("par-display");
@@ -495,7 +426,6 @@ $(document).ready(function(){
 
 					console.log(totalEvents + " Total Events")
 					if (totalEvents > numPerPage){
-<<<<<<< HEAD
 						var numPages;
 						if (totalEvents < numPerPage*10){
 							numPages = Math.ceil(totalEvents/numPerPage)
@@ -520,37 +450,11 @@ $(document).ready(function(){
 							$(".pagination").append(pageList)
 
 						}
-=======
-		      	var numPages;
-		      	if (totalEvents < numPerPage*10){
-	          	numPages = Math.ceil(totalEvents/numPerPage)
-	          	console.log(numPages + " numPages");
-		      	}
-
-			    	else{
-		        	numPages = 10;
-		        	console.log(numPages + " numPages");
-			    	}
-
-
-			    	for (var q = 0; q < numPages; q++) {
-		        	var pageList = $("<li class='page-item'><a class='page-link'>" + parseInt(q+1) + "</a></li>");
-		        	pageList.attr("data-number", q);
-		        	if (q==currentPage){
-		        		pageList.addClass("active")
-		        	}
-          
-          		pageList.attr("id", "page-" + parseInt(q+1))
-          		$(".pagination").append(pageList)
-
-      			}
->>>>>>> 00f5d66c54959d56b77347548b1594829bb29e78
 
     			} // close if totalEvents > numPerPage (pagination)
 
 
 				} // close the else --> when there are events in the response
-<<<<<<< HEAD
 
 				initGoogleMap();
 
@@ -558,17 +462,6 @@ $(document).ready(function(){
 
 				console.log("queryUrl" + queryUrl);
 
-
-=======
-
-				initGoogleMap();
-
-              
-
-				console.log("queryUrl" + queryUrl);
-          
-        
->>>>>>> 00f5d66c54959d56b77347548b1594829bb29e78
 
           }, //close success
           error: function(xhr, status, err) {
@@ -580,7 +473,6 @@ $(document).ready(function(){
 
 
 
-<<<<<<< HEAD
 	$("#submit").on("click", function(event) {
 		event.preventDefault();
 
@@ -633,76 +525,10 @@ $(document).ready(function(){
 		//add class "active" to the page numebr clicked
 		$(this).addClass("active");
 		queryUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apiKey + zipCode + city + state + radius+  keyword + classificationName + size + page;
-=======
- $("#submit").on("click", function(event) {
- 	clearLocations();
-
- 	//every time there is a new search, go to first page of results
- 	currentPage = 0;
- 	
-
- 	// clearEvents();
-
- 	event.preventDefault();
-  var searchInput = $("#search-input").val();
-  // value from dropdown
-  var eventInput = $('#eventType-input').find(":selected").attr("value");
-  console.log("eventInput" + eventInput)
-  
-  cityInput = $("#city-input").val();
-  //&stateCode=il //&city=chicago
-  stateInput = $('#state-input').find(":selected").attr("value");
-  zipInput = $("#zip-input").val();
-  radiusInput = $("#radius-input").val();
-  apiKey = "RElm0QfyEntLwlAvZwiZBD5GExqBRGIO"
-  city = "&city=" + cityInput;
-  state = "&stateCode=" + stateInput;
-  zipCode = "&postalCode=" + zipInput;
-  keyword = "&keyword=" + searchInput; 
-  radius = "&radius=" + radiusInput; 
-  classificationName = "&classificationName=" + eventInput;
-  size = "&size=" + numPerPage;
-  page = "&page=" + currentPage;
- 
-  queryUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apiKey + zipCode + city + state + radius+  keyword + classificationName + size + page;
-
-  ajaxCall();
-
-    
-  console.log("locations: " + [locations])
-	}); //submit click
-
-$("#page-buttons").on("click", "li", function() {
-	currentPage = $(this).attr("data-number");
-
-
-	console.log("currentPage " + currentPage)
-
-        
- 	
- 	// // console.log(($(this).val) + "this.val()")
- 	page = "&page=" + currentPage
- 	
- 	// remove active class from all page numbers 
- 	//(this way we don't have to determine which page number was the last one clicked)
-   $(".page-item").removeClass("active");
-   //add class "active" to the page numebr clicked
-   $(this).addClass("active");
-  queryUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apiKey + zipCode + city + state + radius+  keyword + classificationName + size + page;
-
-  //ajaxCall with the new page number in queryUrl
-  ajaxCall();
-	
-	}); //page-button click
-
-
-
-}); //document.ready
->>>>>>> 00f5d66c54959d56b77347548b1594829bb29e78
 
 		//ajaxCall with the new page number in queryUrl
-		ajaxCall();
-
+  		ajaxCall();
+	
 	}); //page-button click
 
 
